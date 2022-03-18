3097788759437ae1b72b88545d5a716460a82ac4
Aileen Reichelt <reichelt@cl.uni-heidelberg.de>
2022-03-18 19:05:26 +0100

    Clean up useless rewards/events

--- agent_code/coli_agent/train.py
+++ agent_code/coli_agent/train.py
@@ -14,7 +14,6 @@ from agent_code.coli_agent.callbacks import (
     get_neighboring_tiles_until_wall,
     state_to_features,
 )
-from agent_code.coli_agent.plots import get_plots
 
 Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
 
@@ -22,25 +21,8 @@ TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
 
 # --- Custom Events ---
 
-# - Events with direct state feature correspondence -
-
 WAS_BLOCKED = "WAS_BLOCKED"  # tried to move into a wall/crate/enemy/explosion (strong penalty)
 MOVED = "MOVED"  # moved somewhere (and wasn't blocked)
-
-PROGRESSED = "PROGRESSED"  # in last 5 turns, agent visited at least 3 unique tiles
-STAGNATED = "STAGNATED"  # opposite for stronger effect
-
-FLED = "FLED"  # was in "danger zone" of a bomb and moved out of it (reward)
-SUICIDAL = (
-    "SUICIDAL"  # moved from safe field into "danger" zone of bomb (penalty, higher than reward)
-)
-
-DECREASED_DISTANCE = (
-    "DECREASED_DISTANCE"  # decreased length of shortest path to nearest coin or crate BY ONE
-)
-INCREASED_DISTANCE = (
-    "INCREASED_DISTANCE"  # increased length of shortest path to nearest coin or crate BY ONE
-)
 
 FOLLOWED_DIRECTION = "FOLLOWED_DIRECTION"  # went in direction indicated by coin/crate feature
 NOT_FOLLOWED_DIRECTION = "NOT_FOLLOWED_DIRECTION"
@@ -51,17 +33,6 @@ INCREASED_SURROUNDING_CRATES = (
 DECREASED_SURROUNDING_CRATES = (
     "DECREASED_SURROUNDING_CRATES"  # equal or slightly higher penalty for balance
 )
-
-# - Events without direct state feature correspondence -
-
-# idea: Agent-Coin ratio: reward going after crates when there's many coins left (max: 9) and
-# reward going after agents when there aren't
-# idea: reward caging enemies
-
-# more fine-grained bomb area movements: reward/penalize moving one step away from/towards bomb,
-# when agent is already in "danger zone"
-INCREASED_BOMB_DISTANCE = "INCREASED_BOMB_DISTANCE"  # increased or stayed the same
-DECREASED_BOMB_DISTANCE = "DECREASED_BOMB_DISTANCE"
 
 
 def setup_training(self):
@@ -97,23 +68,6 @@ def game_events_occurred(self, old_game_state, self_action: str, new_game_state,
 
     # Custom events and stuff
 
-    if old_feature_dict["bomb_danger_zone"] == 1 and new_feature_dict["bomb_danger_zone"] == 0:
-        events.append(FLED)
-    elif (
-        old_feature_dict["bomb_danger_zone"] == 0
-        and new_feature_dict["bomb_danger_zone"] == 1
-        and not e.BOMB_DROPPED in events
-    ):
-        events.append(SUICIDAL)
-
-    if new_feature_dict["progressed"] == 1 and not e.INVALID_ACTION in events:
-        events.append(PROGRESSED)
-    if new_feature_dict["progressed"] == 0:
-        events.append(STAGNATED)
-
-    if new_game_state["self"][-1] != old_game_state["self"][-1]:
-        events.append(MOVED)
-
     if old_feature_dict["blocked_down"] == 1 and self_action == "DOWN":
         events.append(WAS_BLOCKED)
     elif old_feature_dict["blocked_up"] == 1 and self_action == "UP":
@@ -129,48 +83,6 @@ def game_events_occurred(self, old_game_state, self_action: str, new_game_state,
     new_neighbors = get_neighboring_tiles_until_wall(
         new_game_state["self"][-1], 3, game_state=new_game_state
     )
-
-    crate_counter = [0, 0]  # [old, new]
-    for tile in old_neighbors:
-        if old_game_state["field"][tile[0]][tile[1]] == 1:
-            crate_counter[0] += 1
-    for tile in new_neighbors:
-        if new_game_state["field"][tile[0]][tile[1]] == 1:
-            crate_counter[1] += 1
-    if crate_counter[0] < crate_counter[1]:
-        events.append(INCREASED_SURROUNDING_CRATES)
-    elif crate_counter[0] > crate_counter[1]:
-        events.append(DECREASED_SURROUNDING_CRATES)
-
-    if old_feature_dict["bomb_danger_zone"] == 1:
-        bomb_positions = []
-        for tile in old_neighbors:
-            if tile in [bomb[0] for bomb in old_game_state["bombs"]]:
-                bomb_positions.append(tile)
-        shortest_old_distance = 1000
-        for bp in bomb_positions:
-            distance = abs(
-                sum(np.array(old_game_state["self"][-1]) - np.array(bp))
-            )  # e.g.: [13,8] - [13,10] = [0,-2] -> |-2|
-            if distance < shortest_old_distance:
-                shortest_old_distance = distance
-        for tile in new_neighbors:
-            if tile in [bomb[0] for bomb in new_game_state["bombs"]]:
-                bomb_positions.append(tile)
-        shortest_new_distance = 1000
-        for bp in bomb_positions:
-            distance = abs(sum(np.array(new_game_state["self"][-1]) - np.array(bp)))
-            if distance < shortest_new_distance:
-                shortest_new_distance = distance
-        if shortest_new_distance < shortest_old_distance:
-            events.append(DECREASED_BOMB_DISTANCE)
-        elif shortest_new_distance >= shortest_old_distance:
-            events.append(INCREASED_BOMB_DISTANCE)
-
-    if self.previous_distance < self.current_distance:
-        events.append(DECREASED_DISTANCE)
-    elif self.previous_distance >= self.current_distance and not e.COIN_COLLECTED in events:
-        events.append(INCREASED_DISTANCE)
 
     if old_feature_dict["coin_direction"] == self_action:
         events.append(FOLLOWED_DIRECTION)
@@ -230,39 +142,24 @@ def end_of_round(self, last_game_state, last_action, events):
 
     self.logger.info(f"Total rewards in episode {self.episode}: {self.rewards_of_episode}")
     self.logger.info(f"Final Score: {last_game_state['self'][1]}")
+    self.rewards_of_episode = 0
 
+    if self.episode % 250 == 0 and self.episode != 0:
+        self.logger.info(f"Saving Q-Table at episode: {self.episode}")
+        np.save(f"q_table-{self.timestamp}", self.q_table)
+
+    self.episode += 1
     self.exploration_rate = self.exploration_rate_end + (
         self.exploration_rate_initial - self.exploration_rate_end
     ) * np.exp(
         -self.exploration_decay_rate * self.episode
     )  # decay
 
-    q_table_fraction_unseen_current = fraction_of_unseen_states(self.q_table)
-    q_table_average_seen_current = avg_seen_actions(self.q_table)
-    q_table_distribution_of_actions_current = distribution_of_best_actions(self.q_table)
-
-    self.logger.info(f"Fraction of unseen states: {q_table_fraction_unseen_current}")
-    self.logger.info(f"Average seen actions per state: {q_table_average_seen_current}")
+    self.logger.info(f"Fraction of unseen states: {fraction_of_unseen_states(self.q_table)}")
+    self.logger.info(f"Average seen actions per state: {avg_seen_actions(self.q_table)}")
     self.logger.info(
-        f"Distribution of actions over all states: {q_table_distribution_of_actions_current}"
+        f"Distribution of actions over all states: {distribution_of_best_actions(self.q_table)}"
     )
-    self.q_table_fraction_unseen.append(q_table_fraction_unseen_current)
-    self.q_table_average_seen.append(q_table_average_seen_current)
-
-    self.exploration_rates_of_episodes.append(self.exploration_rate)
-    self.rewards_of_episodes.append(self.rewards_of_episode)
-    self.game_scores_of_episodes.append(last_game_state["self"][1])
-
-    if self.episode % 250 == 0 and self.episode != 0:
-        self.logger.info(f"Saving Q-Table at episode: {self.episode}")
-        np.save(f"q_table-{self.timestamp}", self.q_table)
-
-        self.logger.info(f"Creating plots *after* episode {self.episode}...")
-        self.q_table_distribution_of_actions.append(q_table_distribution_of_actions_current)
-        get_plots(self)
-
-    self.rewards_of_episode = 0
-    self.episode += 1
 
 
 def reward_from_events(self, events: List[str]) -> int:
@@ -271,33 +168,23 @@ def reward_from_events(self, events: List[str]) -> int:
     """
 
     game_rewards = {
-        # e.BOMB_DROPPED: 10,  # adjust aggressiveness
-        # # e.BOMB_EXPLODED: 0,
-        # e.COIN_COLLECTED: 50,
-        # # e.COIN_FOUND: 5,  # direct consequence from crate destroyed, redundant reward?
-        # e.WAITED: -3,  # adjust passivity
-        # e.CRATE_DESTROYED: 4,
-        # e.GOT_KILLED: -50,  # adjust passivity
-        # e.KILLED_OPPONENT: 200,
-        # e.KILLED_SELF: -10,  # you dummy --- this *also* triggers GOT_KILLED
-        # e.OPPONENT_ELIMINATED: 0.05,  # good because less danger or bad because other agent scored points?
-        # # e.SURVIVED_ROUND: 0,  # could possibly lead to not being active - actually penalize if agent too passive?
-        # # necessary? (maybe for penalizing trying to move through walls/crates) - yes, seems to be necessary to
-        # # learn that one cannot place a bomb after another placed bomb is still not exploded
-        # e.INVALID_ACTION: -10,
-        # WAS_BLOCKED: -20,
-        # MOVED: -0.1,
-        # PROGRESSED: 10,  # higher?
-        # STAGNATED: -3,  # higher? lower?
-        # FLED: 15,
-        # SUICIDAL: -15,
-        # DECREASED_DISTANCE: 8,
-        # INCREASED_DISTANCE: -8.1,  # higher? lower? idk
-        # INCREASED_SURROUNDING_CRATES: 1.5,
-        # DECREASED_SURROUNDING_CRATES: -1.6,
-        # INCREASED_BOMB_DISTANCE: 5,
-        # DECREASED_BOMB_DISTANCE: -5.1,
-        FOLLOWED_DIRECTION: 5,  # possibly create penalty
+        e.BOMB_DROPPED: 10,
+        e.BOMB_EXPLODED: 0,
+        e.COIN_COLLECTED: 50,
+        e.COIN_FOUND: 5,
+        e.WAITED: -3,
+        e.CRATE_DESTROYED: 4,
+        e.GOT_KILLED: -50,
+        e.KILLED_OPPONENT: 200,
+        e.KILLED_SELF: -10,
+        e.OPPONENT_ELIMINATED: 0.05,
+        e.SURVIVED_ROUND: 0,
+        e.INVALID_ACTION: -10,
+        WAS_BLOCKED: -20,
+        MOVED: -0.1,
+        INCREASED_SURROUNDING_CRATES: 1.5,
+        DECREASED_SURROUNDING_CRATES: -1.6,
+        FOLLOWED_DIRECTION: 5,
         NOT_FOLLOWED_DIRECTION: -6,
     }
 
