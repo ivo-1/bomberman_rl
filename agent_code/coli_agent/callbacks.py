138d3e68a9e88f7f458a8c75c805548392bf0086
Aileen Reichelt <reichelt@cl.uni-heidelberg.de>
2022-03-27 18:38:17 +0200

    Delete unnecessary tests (which is all tests ...)

--- agent_code/coli_agent/callbacks.py
+++ agent_code/coli_agent/callbacks.py
@@ -839,12 +839,3 @@ def state_to_features(self, game_state: dict) -> int:
     raise ReferenceError(
         "State dict created by state_to_features was not found in self.state_list."
     )
-
-
-# Only to demonstrate test
-class DecisionTransformer:
-    def __init__(self):
-        pass
-
-    def adding_one(self, number: int) -> int:
-        return number + 1
