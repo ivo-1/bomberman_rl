12e3f78b1bdcdb45bf51ba3a2a28a8609fb8027d
Ivo Schaper <ivoschaper@gmail.com>
2022-03-27 13:33:09 +0200

    Add more trajectories ==> 1.000.000 timesteps

--- agent_code/coli_agent_offline/decision_transformer/train.py
+++ agent_code/coli_agent_offline/decision_transformer/train.py
@@ -326,7 +326,7 @@ if __name__ == "__main__":
     parser.add_argument("--weight_decay", "-wd", type=float, default=1e-4)
     parser.add_argument("--warmup_steps", type=int, default=10000)
     parser.add_argument("--num_eval_episodes", type=int, default=100)
-    parser.add_argument("--max_iters", type=int, default=3)
+    parser.add_argument("--max_iters", type=int, default=20)
     parser.add_argument("--num_steps_per_iter", type=int, default=10)
     parser.add_argument(
         "--scale", type=float, default=2.5
