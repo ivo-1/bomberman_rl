15fbf6b0fb99920deaf8a969da9771f4d903cf2c
Ivo Schaper <ivoschaper@gmail.com>
2022-03-31 14:04:37 +0200

    Change filename of log file

--- agent_code/coli_agent_offline/decision_transformer/setup_logger.py
+++ agent_code/coli_agent_offline/decision_transformer/setup_logger.py
@@ -1,6 +1,5 @@
 import logging
 
-logging.basicConfig(filename="logs/coli_agent_offline_train.log")
 dt_logger = logging.getLogger("dt_logger")
 dt_logger.setLevel(logging.INFO)
 handler = logging.FileHandler("logs/coli_agent_offline_train.log", mode="w")
