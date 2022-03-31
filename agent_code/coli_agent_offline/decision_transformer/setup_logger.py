a6c10cdcfdf89321f4fb3bce352999ed996fc31a
Ivo Schaper <ivoschaper@gmail.com>
2022-03-31 13:56:17 +0200

    Overwrite old log file

--- agent_code/coli_agent_offline/decision_transformer/setup_logger.py
+++ agent_code/coli_agent_offline/decision_transformer/setup_logger.py
@@ -1,4 +1,12 @@
 import logging
 
-logging.basicConfig(filename="logs/coli_agent_offline.log", level=logging.DEBUG)
+logging.basicConfig(filename="logs/coli_agent_offline_train.log")
 dt_logger = logging.getLogger("dt_logger")
+dt_logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler("logs/coli_agent_offline_train.log", mode="w")
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)-8s %(name)s:%(lineno)-3s - %(funcName)s()   %(message)s"
+)
+handler.setFormatter(formatter)
+dt_logger.addHandler(handler)
